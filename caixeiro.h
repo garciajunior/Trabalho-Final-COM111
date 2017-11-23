eda56e2f0897e3666734e27a4e3cf87ff3733255
Junior Garcia <jhunior.get@hotmail.com>
2017-11-23 19:03:10 -0200

    Atualizando o .h

--- caixeiro.h
+++ caixeiro.h
@@ -1,17 +1,25 @@
-FILE *abreArq (char *nomeArq, int *vet, int **mat, int *tam);
 
+/*recebe o nome do arquivo e retorna um ponteiro do arquivo
+Atribui o valor da primeira linha na variavel tam*/
+FILE *abreArq (char *nomeArq, int *tam);
+
+/*fecha o arquivo*/
 FILE *fechaArq (FILE *arq);
 
-void forcabruta (int *ciclo, int **mat, int *tam, int pos, int *custoMinimo);
+/*Faz todas as permutações entre as cidades e calcula o custi minimo*/
+void forcabruta (int *ciclo, int **mat, int tam, int pos, int *custoMinimo);
 
-void permuta(int *vetor, int inf, int sup, int *custo);
+//recebe uma permutação e calcula o menor distancia entre as cidades
+int calcCustoMin(int *ciclo, int **m, int tam);
 
+//troca dois elementos do vetor (gera uma permução)
 int *troca(int *vetor, int i, int j);
 
-int **alocaMatriz( int *tam);
+//Aloca a matriz de distancias
+int **alocaMatriz( int tam);
 
-int  *alocaVetor(int *tam);
- 
-void leituraArquivo( int **mat, int * ciclo,  int *tam, FILE *arq);
+//aloca um vetor (ciclo) atribui posições para as cidades
+int  *alocaVetor(int tam);
 
-void menu(char *nomeAqr);
+//recebe um ponteiro do arquivo que contem as distancias entre as cidades e atribui as distancias para as posições da matriz (mat) de distancias
+void leituraArquivo( int **mat,  int tam, FILE *arq);
