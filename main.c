ad62366724e1f1ab6630848d25b5aaaa3526659b
Junior Garcia <jhunior.get@hotmail.com>
2017-11-23 19:03:49 -0200

    Atualizando a main.c

--- main.c
+++ main.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "caixeiro.h"
+#include <string.h>
  
 
 int main()
 {
 //-----------------DECLARAÇÃO DE VARIAVEL----------------------
-	char nomeArq[100];
-	int **mat , *vet , *tam;
-	int custo = 10000;
+	char nomeArq[124];
+	int **mat , *vet , tam, op;
 	FILE *arq;
+	int custoMinimo = 19999;
 //----------------ENTRADA DE DADOS PELO USUARIO-----------------
-	//printf("Informe o nome do arquivo\n");
-	//scanf(" %s", nomeArq);
-
+	/**/
+	//strcpy(nomeArq, "arq10.txt");
 //---------------CHAMADA DE FUNÇOES----------------------------
-        menu(nomeArq);
-	arq = abreArq (nomeArq,vet,mat, tam);
-	vet = alocaVetor(tam);
-	mat = alocaMatriz(tam);
-	leituraArquivo(mat, vet, tam, arq);	
-	int custoMinimo = 10000;
-	forcabruta (vet, mat, tam, 0, &custoMinimo);		
-	printf(" O menor custo entre as cidades e %d\n", custoMinimo);
 
-	
+	do {
+
+		printf("********************\n");
+		printf("*  MENU PRINCIPAL  *\n");
+		printf("********************\n");
+		printf("* 1 - Abrir arquivo*\n");
+		printf("* 2 - sair         *\n");
+		printf("********************\n");
+		scanf (" %d", &op);
+		switch (op){
+			case 1:
+				printf("Informe o nome do arquivo\n");
+				scanf(" %s", nomeArq);
+
+				arq = abreArq (nomeArq, &tam);
+				vet = alocaVetor(tam);
+
+				mat = alocaMatriz(tam);
+				
+				
+				leituraArquivo(mat, tam, arq);
+
+				forcabruta (vet, mat, tam, 0, &custoMinimo);
+					
+				printf(" O menor custo entre as cidades e %d\n", custoMinimo);
+				break;
+			case 2:
+				printf ("Encerrando programa\n");
+				break;
+			default:
+				printf("Entre com a opção correta\n");
+				break;
+		}
+	}while (op != 2);
 	return 0;
 }
